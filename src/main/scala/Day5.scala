47afb738d8874623da2754c19a076990d8d20965
Michael Reitgruber <reitgruber.michael@gmail.com>
2020-12-05 10:49:53 +0100

    fixes sorting weirdness; marginally faster

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -5,12 +5,11 @@ object Day5 {
     val boardingPasses: List[Int] = Util.loadDayLines(5)
       .map(_.replaceAll("[BR]", "1").replaceAll("[FL]", "0"))
       .map(Integer.parseInt(_, 2))
-      .sorted
 
     //Part 1
-    println(boardingPasses.last)
+    println(boardingPasses.max)
     
     //Part2
-    println(boardingPasses.sliding(2).find(i => i(1) - i(0) == 2).get(0) + 1)
+    println(boardingPasses.sorted.sliding(2).find(i => i(1) - i(0) == 2).get(0) + 1)
   }
 }
