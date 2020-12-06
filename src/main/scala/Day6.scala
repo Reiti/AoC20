4e465bc27727a39ef0a56049eae8f4383236f730
Michael Reitgruber <reitgruber.michael@gmail.com>
2020-12-06 09:57:03 +0100

    Day6

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -3,8 +3,8 @@ import util.Util
 object Day6 {
   def main(args: Array[String]): Unit = {
     val answers: Array[Array[Set[Char]]] = Util.loadDay(6)
-                                               .split("\n\n")
-                                               .map(_.split("\n").map(_.toSet))
+      .split("\n\n")
+      .map(_.split("\n").map(_.toSet))
     
     //Part 1
     println(answers.map(_.foldLeft(Set[Char]())(_ union _)).map(_.size).sum)
